7bfc3f31092a308246b6350ffadb5399a4200eb6
Marcos Pereira <marcos.si.pereira@gmail.com>
2021-04-10 21:58:48 -0300

    Try adding CoppeliaSim DH parameters, but they seem to be the modified DH parameters and the DQ does not accept them yet

--- scripts/KukaLBR14R820Robot.py
+++ scripts/KukaLBR14R820Robot.py
@@ -25,11 +25,21 @@ pi2 = pi/2
 class KukaLBR14R820Robot():
     @staticmethod
     def kinematics():
+        # Parameters by hand
         kuka_lbr14_theta = np.array([pi,    pi,     pi,   pi,   pi,    pi,  0])
         kuka_lbr14_d = np.array([0.2075, 0,     0.42, 0,   0.4054, 0,   0.129])
         kuka_lbr14_a = np.array([0,     0,     0,   0,   0,    0,   0])
         kuka_lbr14_alpha = np.array([pi2,   pi2, -pi2, -pi2, pi2, pi2, 0])
         kuka_lbr14_type = np.array([0, 0, 0, 0, 0, 0, 0])
+
+        # Parameters from CoppelliaSim DH extractor
+        # Do not work well
+        # kuka_lbr14_theta = np.array([-pi, -pi, 0, -pi, -pi/2, -pi, 0])
+        # kuka_lbr14_d = np.array([0.2075, 0, 0.42, 0, 0.40, 0, 0.129])
+        # kuka_lbr14_a = np.array([0, 0, 0, 0, 0.0438, 0.0438, 0])
+        # kuka_lbr14_alpha = np.array([pi2, pi2, pi2, pi2, -pi2, -pi2, 0])
+        # kuka_lbr14_type = np.array([0, 0, 0, 0, 0, 0, 0])
+
         kuka_DH_matrix = np.array([kuka_lbr14_theta,
                                    kuka_lbr14_d,
                                    kuka_lbr14_a,
